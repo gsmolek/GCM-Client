481b6e81c9ce4f3bc8a6a12b69ce7a61a6759ddb
Mati <mati.halfa.92@gmail.com>
2019-06-18 00:35:54 +0300

    add employee window
    add addPlace window
    add Main for employee window

---
+++ src/employeeWindow/MainEmployee.java
@@ -0,0 +1,36 @@
+package employeeWindow;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+
+
+public class MainEmployee extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			AnchorPane root = (AnchorPane) loader.load(getClass().getResource("/clientWindow/clientWindow.fxml"));
+			Scene scene = new Scene( root );
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
