b0cd6f555ed5514ec1d02ec501b00e9a0e88d3c2
Mati <mati.halfa.92@gmail.com>
2019-06-27 14:28:14 +0300

    finish working on manager window

--- src/managerWindow/ManagerClientInfoHandler.java
+++ src/managerWindow/ManagerClientInfoHandler.java
@@ -2,7 +2,13 @@ package managerWindow;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.ResourceBundle;
 import java.util.concurrent.TimeUnit;
 
@@ -16,12 +22,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -37,7 +47,9 @@ public class ManagerClientInfoHandler implements Initializable{
     @FXML
     private ComboBox<String> _monthComboBox;
 
-    
+    @FXML
+    private Label _userNamePurchaseLabel;
+
     @FXML
     private Button _userInformationDataButton;
 
@@ -62,8 +74,6 @@ public class ManagerClientInfoHandler implements Initializable{
     @FXML
     private TextField _searchTextField;
 
-    @FXML
-    private Label _userNameLabel;
 
     @FXML
     private TextField _phoneField;
@@ -95,15 +105,45 @@ public class ManagerClientInfoHandler implements Initializable{
     @FXML
     private Pane _paneUserInfoPruchase;
     
-
     @FXML
     private Button _userPruchaseDataButton;
 
+    @FXML
+    private Button _viewPurchaseHistoryButton;
+    
+    @FXML
+    private TableView<PruchaseRowView> _resTable;
+
+    @FXML
+    private DatePicker _toDate;
+    
+    @FXML
+    private DatePicker _fromDate;
+    
+    @FXML
+    private TableColumn<PruchaseRowView, String> _mapCollectionColumn;
+    
+    @FXML
+    private TableColumn<PruchaseRowView, String> _versionColumn;
+    
+    @FXML
+    private TableColumn<PruchaseRowView, String> _typeOfPruchaseColumn;
+
+    @FXML
+    private TableColumn<PruchaseRowView, String> _periodColumn;
+    
+    
     private ArrayList<Object> sendSQL = new ArrayList<Object>();
 	private ChatClient chat = null;
 	private ArrayList<ArrayList<String>> m;
     
 	ObservableList<String> listToShow = FXCollections.observableArrayList();
+	
+	private final String GET_DATA = "2";
+	private final String UPDATE_DELETE_INSERT_DATA = "3";
+	private final String GET_IMAGE = "5";
+	
+	private static String _idClient ;
 	
     @FXML
     void clickSearchUser(ActionEvent event)
@@ -112,17 +152,17 @@ public class ManagerClientInfoHandler implements Initializable{
 		String sql=null;
 		String textToSearch = _searchTextField.getText();
 		
-		sql = "SELECT first_name,last_name,user_name,password,email,phone,creditcard,expirationM,expirationY,cvv \r\n" + 
+		sql = "SELECT first_name,last_name,user_name,password,email,phone,creditcard,expirationM,expirationY,cvv,Id \r\n" + 
 				"FROM gcm.users,gcm.user_card \r\n" + 
 				"Where user_id=Id AND (email=\""+ textToSearch +"\" OR user_name=\"" + textToSearch + "\");";
 		
-		sendSQL.add("2");
+		sendSQL.add(GET_DATA);
 		sendSQL.add(sql);
 		chat.handleMessageFromClient(sendSQL);
 		try {
 			TimeUnit.MILLISECONDS.sleep(100);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+		} catch (InterruptedException e) 
+		{
 			e.printStackTrace();
 		}
 		
@@ -138,7 +178,7 @@ public class ManagerClientInfoHandler implements Initializable{
 			_userInformationDataButton.setVisible(false);
 			_userPruchaseDataButton.setVisible(false);
 			
-			
+			/*
 			_firstNameField.setText("");
 			_lastNameField.setText("");
 			_userNameField.setText("");
@@ -149,7 +189,7 @@ public class ManagerClientInfoHandler implements Initializable{
 			_cardNumberField.setText("");
 			_monthComboBox.setValue("mm");
 			_yearComboBox.setValue("yyyy");
-			_cVVField.setText("");
+			_cVVField.setText("");*/
 		}
 		else 
 		{
@@ -158,38 +198,26 @@ public class ManagerClientInfoHandler implements Initializable{
 			_userInformationDataButton.setVisible(true);
 			_userPruchaseDataButton.setVisible(true);
 			
-			for (ArrayList<String> arrayList : m)
-			{
-				_firstNameField.setText(arrayList.get(0));
-				_lastNameField.setText(arrayList.get(1));
-				_userNameField.setText(arrayList.get(2));
-				_passwordField.setText(arrayList.get(3));
-				
-				_emailField.setText(arrayList.get(4));
-				_phoneField.setText(arrayList.get(5));
-				_cardNumberField.setText(arrayList.get(6));
-				_monthComboBox.setValue(arrayList.get(7));
-				_yearComboBox.setValue(arrayList.get(8));
-				_cVVField.setText(arrayList.get(9));
-			}
+			
+			ArrayList<String> arrayList = m.get(0);
+			
+			_firstNameField.setText(arrayList.get(0));
+			
+			_lastNameField.setText(arrayList.get(1));
+			_userNameField.setText(arrayList.get(2));
+			_passwordField.setText(arrayList.get(3));
+			_emailField.setText(arrayList.get(4));
+			_phoneField.setText(arrayList.get(5));
+			_cardNumberField.setText(arrayList.get(6));
+			_monthComboBox.setValue(arrayList.get(7));
+			_yearComboBox.setValue(arrayList.get(8));
+			_cVVField.setText(arrayList.get(9));
+			
+			_idClient = arrayList.get(10);
+			
+			
 			System.out.println("success");
 		}
-			
-			
-		//sql = "SELECT * FROM " + table + " WHERE email = '" + textToSearch + "';";
-			
-		
-		
-		///////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////
-
-		
-		
-		/*
-		 * byte[][] result = chat.returnByteArray(); String s = new String(result[0]);
-		 * System.out.println("main: "+s); s = new String(result[1]);
-		 * System.out.println("main: "+s);
-		 */
 
     }
     
@@ -205,8 +233,99 @@ public class ManagerClientInfoHandler implements Initializable{
     {
     	_paneUserInfo.setVisible(false);
     	_paneUserInfoPruchase.setVisible(true);
+    	
+    	_userNamePurchaseLabel.setText(_userNameField.getText());
     }
+   
+    @FXML
+    void clickUpdateUserInformationButton(ActionEvent event)
+    {
+    	sendSQL.clear();
+    	String sql = null; 
+    	
+    	sql="UPDATE user_card,users\r\n" + 
+    			"SET\r\n" + 
+    			"first_name=\" "+ _firstNameField.getText()
+    			+"\",last_name=\""+ _lastNameField.getText() 
+    			+"\",user_name=\""+ _userNameField.getText()
+    			+"\",password=\""+ _passwordField.getText()
+    			+"\",email = \""+ _emailField.getText()
+    			+"\",phone = \""+ _phoneField.getText()
+    			+"\",creditcard=\""+ _cardNumberField.getText()
+    			+"\", expirationM=\""+ _monthComboBox.getValue()
+    			+"\",expirationY=\""+ _yearComboBox.getValue()
+    			+"\",cvv=\""+ _cVVField.getText() +"\"\r\n" + 
+    			"WHERE (user_id = \""+ _idClient +"\" AND user_id=Id);";
+    	
+    	sendSQL.add(UPDATE_DELETE_INSERT_DATA);
+		sendSQL.add(sql);
+		chat.handleMessageFromClient(sendSQL);
+		try {
+			TimeUnit.MILLISECONDS.sleep(100);
+		} catch (InterruptedException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		m = chat.getArray();
+		if(m.size()>0)
+		{
+			System.out.println(m.get(0).get(0));
+		}
+		
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("Update");
+		alert.setContentText("You just update " + _userNameField.getText() +" data");
+		alert.showAndWait();
+    	
+    }
+
+    @FXML
+    void clickViewPurchaseHistoryButton()
+    {
     
+    	
+    	sendSQL.clear();
+		String sql=null;
+	
+		sql = "SELECT version,collaction_id,type_of_purchases,date_end\r\n" + 
+				"FROM gcm.purchases ,map_collection\r\n" + 
+				"Where user_id="+ _idClient +" AND "
+						+ "collaction_id=map_collection.Id AND"
+						+ " date_buy >= \""+_fromDate.getValue().format(DateTimeFormatter.ISO_LOCAL_DATE)+"\" AND date_buy <= \""+_toDate.getValue().format(DateTimeFormatter.ISO_LOCAL_DATE) +"\";";
+		
+		System.out.println(sql);
+		
+		sendSQL.add(GET_DATA);
+		sendSQL.add(sql);
+		chat.handleMessageFromClient(sendSQL);
+		try {
+			TimeUnit.MILLISECONDS.sleep(100);
+		} catch (InterruptedException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		m = chat.getArray();
+		
+		if (m == null || m.isEmpty())
+		{
+			Alert alert = new Alert(Alert.AlertType.INFORMATION);
+			alert.setTitle("Find nothing");
+			alert.setContentText("We didt find a thing!");
+			alert.showAndWait();
+		}
+		else 
+		{
+			PruchaseRowView pruchaseRowView;
+			
+			for (ArrayList<String> arrayList : m) 
+			{
+				pruchaseRowView = new PruchaseRowView (arrayList.get(0),arrayList.get(1),arrayList.get(2),arrayList.get(3));
+				_resTable.getItems().addAll(pruchaseRowView);
+			}
+		}
+    }
     
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1)
@@ -227,6 +346,90 @@ public class ManagerClientInfoHandler implements Initializable{
 			// return false;
 		}
 		
+		_mapCollectionColumn.setCellValueFactory(new PropertyValueFactory<>("mapCollection"));
+		_versionColumn.setCellValueFactory(new PropertyValueFactory<>("version"));
+		_typeOfPruchaseColumn.setCellValueFactory(new PropertyValueFactory<>("typeOfPruchase"));
+		_periodColumn.setCellValueFactory(new PropertyValueFactory<>("Period"));
+		
+		_periodColumn.setText("Until");
 	}
 
+	public class PruchaseRowView {
+	   
+		private String version;
+	    private String mapCollection;
+	    private String typeOfPruchase;
+	    private String period;
+	    
+	    
+		public PruchaseRowView(String version, String mapCollection, String typeOfPruchase, String periodEnd) {
+			super();
+			this.version = version;
+			this.mapCollection = mapCollection;
+			this.typeOfPruchase = typeOfPruchase;
+			this.period = periodEnd;
+			
+		}
+		
+		
+		/**
+		 * @return the version
+		 */
+		public String getVersion() {
+			return version;
+		}
+
+		/**
+		 * @return the mapCollection
+		 */
+		public String getMapCollection() {
+			return mapCollection;
+		}
+
+		/**
+		 * @return the typeOfPruchase
+		 */
+		public String getTypeOfPruchase() {
+			return typeOfPruchase;
+		}
+
+		/**
+		 * @return the period
+		 */
+		public String getPeriod() {
+			return period;
+		}
+
+		/**
+		 * @param version the version to set
+		 */
+		public void setVersion(String version) {
+			this.version = version;
+		}
+
+		/**
+		 * @param mapCollection the mapCollection to set
+		 */
+		public void setMapCollection(String mapCollection) {
+			this.mapCollection = mapCollection;
+		}
+
+		/**
+		 * @param typeOfPruchase the typeOfPruchase to set
+		 */
+		public void setTypeOfPruchase(String typeOfPruchase) {
+			this.typeOfPruchase = typeOfPruchase;
+		}
+
+		/**
+		 * @param period the period to set
+		 */
+		public void setPeriod(String period) {
+			this.period = period;
+		}
+
+		
+
+	
+	}
 }
